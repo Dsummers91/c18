7bba56062589473074d1f724d43ee84ff718d3cb
Deon <deonsummers91@gmail.com>
2018-05-12 16:00:46 -0500

    bleh

--- server.js
+++ server.js
@@ -14,8 +14,22 @@ app.use(bodyParser.json());
 
 app.get('/test', async(req, res) => {
 	let tokenCount = await foodChainz.tokensCreated();
+  console.log(web3.eth.coinbase);
 	res.send({ tokenCount });
 });
+
+app.get('/explorer', (req, res) => {
+  foodChainz.allEvents({}, {fromBlock: 0, toBlock: 'latest'}).get((err, events) => {
+    res.send(events);
+  });
+});
+
+app.get('/purchase', (req, res) => {
+  return foodChainz.contract.createShipment([1,2,3,4,5], web3.eth.coinbase, {from: web3.eth.coinbase, gas: 5000000}, (err, resp) => {
+    res.send({ tx: resp });
+  });
+});
+
 
 server = app.listen(process.env.PORT || 8080, async() => {
 	foodChainz = await FoodChainz.deployed();
