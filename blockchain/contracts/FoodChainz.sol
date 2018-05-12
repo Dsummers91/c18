e29e34603ff607dc68bd85e61953b5ddb585db61
Deon <deonsummers91@gmail.com>
2018-05-12 17:46:12 -0500

    i dunno

--- blockchain/contracts/FoodChainz.sol
+++ blockchain/contracts/FoodChainz.sol
@@ -4,16 +4,46 @@ import './tokens/ERC721BasicToken.sol';
 
 contract FoodChainz is ERC721BasicToken() {
   uint256 public tokensCreated;
+  mapping(uint256 => Status) public status;
 
-  constructor() public {
+  enum Status {
+    Uninitiated,
+    Created,
+    Confirmed,
+    Completed
+  }
+
+  event ShipmentCreated(uint256 token, address owner);
+  event ShipmentConfirmed(uint256 token, address owner);
+  event ShipmentCompleted(uint256 token, address owner);
+
+  constructor() public {}
+
+  function statusOf(uint256 _token) public view returns(uint256) {
+    return uint256(status[_token]);
   }
 
   // max 5 containers per shipment
   function createShipment(uint256[5] _tokens, address _owner) public {
     for(uint256 i=0; i<_tokens.length; i++) {
       if(_tokens[i] == uint256(0)) { break; }
+      status[_tokens[i]] = Status.Created;
       super._mint(_owner, _tokens[i]);
       tokensCreated++;
     }
+  }
+
+  function confirmShipment(uint256[5] _tokens) public {
+    for(uint256 i=0; i<_tokens.length; i++) {
+      if(_tokens[i] == uint256(0)) { break; }
+      status[_tokens[i]] = Status.Confirmed;
+    } 
+  }
+
+  function completeShipment(uint256[5] _tokens) public {
+    for(uint256 i=0; i<_tokens.length; i++) {
+      if(_tokens[i] == uint256(0)) { break; }
+      status[_tokens[i]] = Status.Completed;
+    } 
   }
 }
--- blockchain/test/foodchainz.test.js
+++ blockchain/test/foodchainz.test.js
@@ -11,12 +11,33 @@ contract('FoodChainz', () => {
   it('should be able to create a shipment', async() => {
     await foodChainz.createShipment([2,3,4,5,6], web3.eth.coinbase)
     let tokensCreated = await foodChainz.tokensCreated();
+    let tokenStatus = await foodChainz.statusOf(2);
     assert.equal(+tokensCreated, 5);
+    assert.equal(+tokenStatus, 1)
   });
 
   it('should be able to create a smaller shipment', async() => {
     await foodChainz.createShipment([2,3,4], web3.eth.coinbase)
     let tokensCreated = await foodChainz.tokensCreated();
     assert.equal(+tokensCreated, 3);
+  });
+
+  it('should be able to confirm a smaller shipment', async() => {
+    await foodChainz.createShipment([2,3,4], web3.eth.coinbase)
+    await foodChainz.confirmShipment([2,3,4]);
+    let tokensCreated = await foodChainz.tokensCreated();
+    let tokenStatus = await foodChainz.statusOf(2);
+    assert.equal(+tokensCreated, 3);
+    assert.equal(+tokenStatus, 2)
+  });
+
+  it('should be able to complete a smaller shipment', async() => {
+    await foodChainz.createShipment([2,3,4], web3.eth.coinbase)
+    await foodChainz.confirmShipment([2,3,4]);
+    await foodChainz.completeShipment([2,3,4]);
+    let tokensCreated = await foodChainz.tokensCreated();
+    let tokenStatus = await foodChainz.statusOf(2);
+    assert.equal(+tokensCreated, 3);
+    assert.equal(+tokenStatus, 3)
   });
 });
